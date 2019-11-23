7d958e52947637b8608c0cf640f9d97c23057d0f
huanghaishen <127.0.0.8@163.com>
2019-11-23 16:20:46 +0800

    commited by sam

---
+++ pca/linear_regre.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from PIL import Image
+import numpy as np
+
+
+# 线性回归
+# 采用两种方法求解
+# 第一种 用矩阵求逆的方法来求解W
+# 第二种 用梯度下降的方法来求解W
+
+
+if __name__ == '__main__':
+    print "hello world"
