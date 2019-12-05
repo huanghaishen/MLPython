dfb8ed77658cb07e215f9dbab8e4bbe1c8592cc7
huanghaishen <127.0.0.8@163.com>
2019-12-05 18:13:02 +0800

    commited by sam

--- line/linear_regre.py
+++ line/linear_regre.py
@@ -3,6 +3,7 @@
 from PIL import Image
 import pandas as pd
 import numpy as np
+import math
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
@@ -64,37 +65,32 @@ def linear_matrix():
     x = data[feature_cols]
     y = data['sales']
 
-
-
     # 将数据进行归一化
-    minMax = MinMaxScaler()
-    x = minMax.fit_transform(x)
+    minmax = MinMaxScaler()
+    x = minmax.fit_transform(x)
 
     # x添加bias项
     len = np.shape(x)[0]
     bx = np.ones(shape=[len, 1], dtype=int)
-    # x = np.append(x,bx,axis=1)
     x = np.hstack((x, bx))
 
-
-    #函数用于将矩阵随机划分为训练子集和测试子集，并返回划分好的训练集测试集样本和训练集测试集标签
+    # 函数用于将矩阵随机划分为训练子集和测试子集，并返回划分好的训练集测试集样本和训练集测试集标签
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
 
-    #参数计算公式 θ=（X^TX)^-1X^TY
-    #x_train=np.matrix(x_train)
-    #y_train = np.matrix(y_train)
-    xtx=np.dot(x_train.T,x_train)
-    xtx_1=np.linalg.inv(xtx)
+    # 参数计算公式 θ=（X^TX)^-1X^TY
+    xtx = np.dot(x_train.T,x_train)
 
-    xy=np.dot(x_train.T,y_train)
-    W=np.dot(xtx_1,xy)
+    # 矩阵求逆
+    xtx_inv = np.linalg.inv(xtx)
+
+    xy = np.dot(x_train.T,y_train)
+    W = np.dot(xtx_inv,xy)
 
     y_hat = np.dot(x_test,W.T)
 
-
-    # plot y_label/y_predict
-    len=np.shape(x_test)[0]
-    t = np.arange(len)
+    # 显示测试数据和预测数据
+    len_test = np.shape(x_test)[0]
+    t = np.arange(len_test)
     plt.plot(t, y_test, 'r-', linewidth=2, label='Test(label)')
     plt.plot(t, y_hat, 'g-', linewidth=2, label='Predict')
     plt.legend()
@@ -117,34 +113,32 @@ def linear_gradient():
     b=[]
 
     rate=0.01
-    last_total_error=-1.0
+    last_total_error=0
 
     # 将数据进行归一化
-    minMax = MinMaxScaler()
-    x = minMax.fit_transform(x)
+    minmax = MinMaxScaler()
+    x = minmax.fit_transform(x)
 
 
 
-    #函数用于将矩阵随机划分为训练子集和测试子集，并返回划分好的训练集测试集样本和训练集测试集标签
+    # 函数用于将矩阵随机划分为训练子集和测试子集，并返回划分好的训练集测试集样本和训练集测试集标签
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
-    len = np.shape(x_test)[0]
+
     num = np.shape(x_test)[1]
     w.append(np.random.uniform(-0.2, 0.2, num))
     b.append(np.random.uniform(-0.2, 0.2, 1))
 
     w=np.matrix(w)
-
-
-    lent = np.shape(y_train)[0]
+    len_test = np.shape(y_train)[0]
     y_train=np.dot(y_train,1)
-    y_train=np.reshape(y_train,(lent,1))
+    y_train=np.reshape(y_train,(len_test,1))
 
     lentest = np.shape(x_test)[0]
-    #梯度下降，梯度乘以学习率
+    # 梯度下降，梯度乘以学习率
     epoch=1
     while True:
         # 每次只用一个样本训练，随机梯度下降
-        for m in range(lent):
+        for m in range(lentest):
             x_samble=x_train[m]
             y_samble=y_train[m]
             fx = np.dot(x_samble, w.T)+b
@@ -156,34 +150,30 @@ def linear_gradient():
             grad_w = x_samble*delta_error
             grad_b = delta_error
 
-            #更新权重和偏置项
+            # 更新权重和偏置项
             w=w-np.dot(rate,grad_w)
             b=b-np.dot(rate,grad_b)
-        #检测总能的误差是否在下降，如果不下降，或者反而上升，则退出训练
+        # 检测总能的误差是否在下降，如果不下降，或者反而上升，则退出训练
         y_hat=np.dot(x_test,w.T)+b
         y_test_array=np.array(y_test)
         y_test_array=np.reshape(y_test_array,(lentest,1))
 
-        #统计总误差
+        # 统计总误差
         total_error=y_hat-y_test_array
         total_error=np.dot(total_error.T,total_error).sum()
 
         print "epoch:"+str(epoch)+"    last_total_error:" + str(last_total_error) + " total_error:" + str(total_error)
         epoch=epoch+1
-        if(last_total_error<=total_error and last_total_error>0 ):
-            # 总误差不变，或者变大了，退出训练
+        if math.fabs( last_total_error - total_error ) < 0.000000001:
+            # 总误差变化很小，退出训练
             print "training finished"
             break
         else:
             last_total_error=total_error
 
-
-
-
     y_hat = np.dot(x_test, w.T)+b
     y_hat=np.array(y_hat)
-    # plot y_label/y_predict
-
+    # 显示测试数据和训练数据
     t = np.arange(lentest)
     plt.plot(t, y_test, 'r-', linewidth=2, label='Test(label)')
     plt.plot(t, y_hat, 'g-', linewidth=2, label='Predict')
